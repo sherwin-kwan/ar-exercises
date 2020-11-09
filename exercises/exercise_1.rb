b0ec61abc77fe9d512690600188a4c59d9e52d9c
Sherwin Kwan <sherwin.kwan92@gmail.com>
2020-11-09 23:45:23 +0000

    finish exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,38 @@
-require_relative '../setup'
+require_relative "../setup"
 
 puts "Exercise 1"
 puts "----------"
 
+# SCHEMA FOR STORE
+# Column     |            Type             |                      Modifiers
+# ----------------+-----------------------------+-----------------------------------------------------
+#  id             | integer                     | not null default nextval('stores_id_seq'::regclass)
+#  name           | character varying           |
+#  annual_revenue | integer                     |
+#  mens_apparel   | boolean                     |
+#  womens_apparel | boolean                     |
+#  created_at     | timestamp without time zone | not null
+#  updated_at     | timestamp without time zone | not null
+
 # Your code goes below here ...
+
+arr = [{
+  name: 'Burnaby',
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true
+}, {
+  name: 'Richmond',
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  womens_apparel: true
+}, {
+  name: 'Gastown',
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false
+}]
+
+Store.create(arr)
+
+puts "There are #{Store.count} stores in the database"
