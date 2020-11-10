f03af4b7ee640684ad3d6ca85013a443cf30094a
Sherwin Kwan <sherwin.kwan92@gmail.com>
2020-11-10 00:31:17 +0000

    add exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,3 +10,13 @@ puts "----------"
 
 # Your code goes here ...
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Abe", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Betty", last_name: "Max", hourly_rate: 70)
+@store1.employees.create(first_name: "Charlie", last_name: "London", hourly_rate: 80)
+@store1.employees.create(first_name: "Debbie", last_name: "Paris", hourly_rate: 90)
+@store1.employees.create(first_name: "Earl", last_name: "Rome", hourly_rate: 100)
+@store2.employees.create(first_name: "Federica", last_name: "Virani", hourly_rate: 110)
+@store2.employees.create(first_name: "George", last_name: "Max", hourly_rate: 120)
+@store2.employees.create(first_name: "Hillary", last_name: "London", hourly_rate: 130)
+@store2.employees.create(first_name: "Ian", last_name: "Paris", hourly_rate: 140)
+@store2.employees.create(first_name: "Jill", last_name: "Rome", hourly_rate: 150)
