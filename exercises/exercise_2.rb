718e50971c8ada02f70dbd5b24db3624944b5e6b
Sherwin Kwan <sherwin.kwan92@gmail.com>
2020-11-10 00:13:22 +0000

    add exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.name = "New Burnaby"
+@store1.save 
+
+puts "Store 1's name is: #{Store.find_by(id: 1).name}" 
