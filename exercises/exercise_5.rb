2ffad8c9c3ee3aa1f3b2f1bfbcacff8878fd483b
Sherwin Kwan <sherwin.kwan92@gmail.com>
2020-11-10 00:28:09 +0000

    add exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue is: #{total_revenue}"
+
+puts "Average revenue is: #{total_revenue/Store.count}"
+
+# Find number of well-performing stores
+star_performers = Store.where("annual_revenue > 1000000")
+puts "There are #{star_performers.count} stores performing well with revenue over $1 million"
