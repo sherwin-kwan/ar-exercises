49a6e61b1e2cee3201bcc04136cca95102bf1642
Sherwin <sherwin.kwan92@gmail.com>
2020-11-09 16:22:08 -0800

    add exercises 3 and 4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -31,7 +31,22 @@ arr = [{
   annual_revenue: 190000,
   mens_apparel: true,
   womens_apparel: false
-}]
+}, {
+  name: 'Surrey',
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true
+}, {
+  name: 'Whistler',
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+}, {
+  name: 'Yaletown',
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+}]     
 
 Store.create(arr)
 
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,13 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+@mens_stores = Store.where(mens_apparel: true)
+puts "Men's Stores"
+@mens_stores.each do |store|
+  puts("Name: #{store.name}, Annual Revenue: #{store.annual_revenue}")
+end
+
+puts "Women's Stores"
+Store.where("womens_apparel = true AND annual_revenue < 1000000").each do |store|
+  puts("Name: #{store.name}, Annual Revenue: #{store.annual_revenue}") 
+end
